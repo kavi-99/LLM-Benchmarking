d8c6eac3d653c338c48174937d501bc546e3b582
#KAVITA SRIRAM# <kavi0008@asp2a-login-ntu01.head.cm.asp2a.nscc.sg>
2025-03-03 23:42:04 +0800

    update vllm model name

--- providers/vllm_provider.py
+++ providers/vllm_provider.py
@@ -20,7 +20,7 @@ class vLLM(ProviderInterface):
 
         # Define available models
         self.model_map = {
-            "common-model": "meta-llama/Llama-3.3-70B-Instruct",
+            "common-model": "./../scratch/models--meta-llama--Llama-3.3-70B-Instruct/snapshots/6f6073b423013f6a7d4d9f39144961bfbfbc386b",
             "common-model-small": "meta-llama/Llama-3.1-8B"
         }
 
@@ -76,6 +76,7 @@ class vLLM(ProviderInterface):
         generated_text = ""
 
         try:
+            print(f"http://{vllm_ip}:{self.vllm_port}/v1/completions")
             start_time = time.perf_counter()
             response = requests.post(
                 f"http://{vllm_ip}:{self.vllm_port}/v1/completions",
